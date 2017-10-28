763e5cdfc0f7538a951cbfe8bd66a42bbd2bdb85
AshBardhan <ashbardhan@gmail.com>
2017-10-28 21:45:11 +0530

    Add GA Tracking Event

--- src/background/main.js
+++ src/background/main.js
@@ -1,3 +1,32 @@
+/**
+ * Add your Analytics tracking ID here.
+ */
+var _AnalyticsCode = 'UA-54569024-2';
+
+/**
+ * Below is a modified version of the Google Analytics asynchronous tracking
+ * code snippet.  It has been modified to pull the HTTPS version of ga.js
+ * instead of the default HTTP version.  It is recommended that you use this
+ * snippet instead of the standard tracking snippet provided when setting up
+ * a Google Analytics account.
+ */
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', _AnalyticsCode]);
+_gaq.push(['_trackPageview']);
+
+(function () {
+	var ga = document.createElement('script');
+	ga.type = 'text/javascript';
+	ga.async = true;
+	ga.src = 'https://ssl.google-analytics.com/ga.js';
+	var s = document.getElementsByTagName('script')[0];
+	s.parentNode.insertBefore(ga, s);
+})();
+
+function trackGAEvent(eventName, eventValue) {
+	_gaq.push(['_trackEvent', eventName, eventValue || '']);
+}
+
 chrome.contextMenus.create({
 	id: "myContextMenu",
 	title: "Memefy This Image",
@@ -7,9 +36,9 @@ chrome.contextMenus.create({
 chrome.contextMenus.onClicked.addListener(function (info, tab) {
 	if (tab) {
 		chrome.tabs.sendMessage(tab.id, {text: "make_meme"}, function (response) {
+			trackGAEvent('menu-option', 'select');
 			console.log(response);
 		});
-
 	}
 });
 
