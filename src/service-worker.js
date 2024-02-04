e32f713bd0c4809eb4bc363c5cfa53b8b1d917c4
AshBardhan <ashbardhan@gmail.com>
2024-02-04 18:14:07 +0530

    Initiate GA4 Tracking Integration

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,20 +1,4 @@
-// var _AnalyticsCode = 'UA-54569024-2';
-// var _gaq = _gaq || [];
-// _gaq.push(['_setAccount', _AnalyticsCode]);
-// _gaq.push(['_trackPageview']);
-
-// (function () {
-// 	var ga = document.createElement('script');
-// 	ga.type = 'text/javascript';
-// 	ga.async = true;
-// 	ga.src = 'https://ssl.google-analytics.com/ga.js';
-// 	var s = document.getElementsByTagName('script')[0];
-// 	s.parentNode.insertBefore(ga, s);
-// })();
-
-// function trackGAEvent(eventName, eventValue) {
-// 	_gaq.push(['_trackEvent', eventName, eventValue]);
-// }
+import Analytics from './utils/google-analytics.js';
 
 chrome.contextMenus.create({
 	id: "myContextMenu",
@@ -26,7 +10,7 @@ chrome.contextMenus.create({
 chrome.contextMenus.onClicked.addListener((info, tab) => {
 	if (tab) {
 		chrome.tabs.sendMessage(tab.id, {text: "make_meme"}, response => {
-			//trackGAEvent('meme_menu-option', 'select');
+			Analytics.fireEvent('meme_menu-option', {value: 'select'});
 			return true;
 		});
 	}
@@ -44,7 +28,7 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 		}
 
 		if (request.msg === 'track_GA_event') {
-			//trackGAEvent(request.eventName, request.eventValue);
+			Analytics.fireEvent(request.eventName, {value: request.eventValue});
 		}
 	}
 });
