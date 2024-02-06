ea24039a5e0e1d51f2af9e0f54bfd2252e4b8d65
AshBardhan <ashbardhan@gmail.com>
2024-02-06 13:36:00 +0530

    Code refactoring and commenting 'service-worker' file

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,12 +1,13 @@
 import Analytics from './utils/google-analytics.js';
 
+// Creates a context menu option that allows your generate meme from the selected image
 chrome.contextMenus.create({
 	id: "myContextMenu",
 	title: "Memefy This Image",
 	contexts: ["image"]
 });
 
-
+// Event listener once the context menu option is selected
 chrome.contextMenus.onClicked.addListener((info, tab) => {
 	if (tab) {
 		chrome.tabs.sendMessage(tab.id, {text: "make_meme"}, response => {
@@ -16,14 +17,11 @@ chrome.contextMenus.onClicked.addListener((info, tab) => {
 	}
 });
 
+// Event listener that is sent from 'content_scripts' to download the generated meme or track GA event
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-	if (request.msg) {
+	if (request?.msg) {
 		if (request.msg === 'download_meme') {
-			chrome.tabs.captureVisibleTab(
-				null,
-				{format: 'png', quality: 100},
-				dataURL => sendResponse({imgSrc: dataURL})
-			);
+			chrome.tabs.captureVisibleTab(null, {format: 'png', quality: 100}, dataURL => sendResponse({imgSrc: dataURL}));
 			return true;
 		}
 
